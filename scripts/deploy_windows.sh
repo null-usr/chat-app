bcf395474644919d51ea0d883d421a86af5b6df0
ferrohound <ntclarke33@gmail.com>
2021-08-04 22:55:43 +0200

    fix typo in deploy script

--- scripts/deploy_windows.sh
+++ scripts/deploy_windows.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-cd wwww
+cd www
 git init
 heroku repo:reset --app scream-into-void && heroku git:remote -a scream-into-void
 git add . && git commit -m "Deploying" && git push heroku main
